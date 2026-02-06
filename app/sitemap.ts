46bf9585753eb894c5cec8175b99aff8e03b4a65
Nikhil Parmar <128135551+nikhilp1234567@users.noreply.github.com>
2026-02-06 14:07:45 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://nikhilp.online/greenrepo'
+  const baseUrl = 'https://www.nikhilp.online/greenrepo'
   
   return [
     {
